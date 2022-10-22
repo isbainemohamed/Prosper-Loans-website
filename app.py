931a994af6c91ee033727bf7c25db81d37509fd6
ISBAINE MOHAMED <isbainemouhamed@gmail.com>
2022-10-22 20:15:00 +0100

    update3

--- app.py
+++ app.py
@@ -8,8 +8,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def home():  # put application's code here
+    return render_template('home.html')
+
+@app.route('/predict')
+def predict():  # put application's code here
     return render_template('index.html')
+
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html')
+
+
 
 
 if __name__ == '__main__':
